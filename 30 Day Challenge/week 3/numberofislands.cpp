26ff79b70f432b035b8cfc617906ce12844e3d93
Yashraj Baheti <35994639+yashraj1120@users.noreply.github.com>
2020-04-18 21:22:17 +0530

    ?

--- 30 Day Challenge/week 3/numberofislands.cpp
+++ 30 Day Challenge/week 3/numberofislands.cpp
@@ -5,7 +5,6 @@ public:
 		{
 			return 0;
 		}
-		vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
 		int res = 0;
 		int rows = grid.size();
 		int cols = grid[0].size();
@@ -13,21 +12,18 @@ public:
 		{
 			for (int j = 0; j < cols; j++)
 			{
-				if (visited[i][j] == false)
+				if (grid[i][j] == '1')
 				{
-					if (grid[i][j] == '1')
-					{
-						visited[i][j] = true;
-						dfs(grid, visited, i, j);
-						++res;
-					}
+					visited[i][j] = true;
+					dfs(grid, i, j);
+					++res;
 				}
 			}
 		}
 		return res;
 
 	}
-	void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int r, int c)
+	void dfs(vector<vector<char>>& grid, int r, int c)
 	{
 		vector<pair<int, int>> points_stack;
 		do
@@ -39,49 +35,27 @@ public:
 				c = x.second;
 				points_stack.pop_back();
 			}
-			if (r - 1 >= 0 && visited[r - 1][c] == false && grid[r - 1][c] == '1')
+			if (r - 1 >= 0 && grid[r - 1][c] == '1')
 			{
-				visited[r - 1][c] = true;
+				grid[r - 1][c] = '!';
 				points_stack.push_back(make_pair(r - 1, c));
 			}
-			if (r + 1 < grid.size() && visited[r + 1][c] == false && grid[r + 1][c] == '1')
+			if (r + 1 < grid.size() && grid[r + 1][c] == '1')
 			{
-				visited[r + 1][c] = true;
+				grid[r + 1][c] = '!';
 				points_stack.push_back(make_pair(r + 1, c));
 			}
-			if (c - 1 >= 0 && visited[r][c - 1] == false && grid[r][c - 1] == '1')
+			if (c - 1 >= 0 && grid[r][c - 1] == '1')
 			{
-				visited[r][c - 1] = true;
+				grid[r][c - 1] = '!';
 				points_stack.push_back(make_pair(r, c - 1));
 			}
-			if (c + 1 < grid[0].size() && visited[r][c + 1] == false && grid[r][c + 1] == '1')
+			if (c + 1 < grid[0].size() && grid[r][c + 1] == '1')
 			{
-				visited[r][c + 1] = true;
+				grid[r][c + 1] = '!';
 				points_stack.push_back(make_pair(r, c + 1));
 			}
 		} while (!points_stack.empty());
 
-
-
-		/*if (r - 1 >= 0 && visited[r - 1][c] == false && grid[r - 1][c] == '1')
-		{
-			visited[r - 1][c] = true;
-			dfs(grid, visited, r - 1, c);
-		}
-		if (r + 1 < grid.size() && visited[r + 1][c] == false && grid[r + 1][c] == '1')
-		{
-			visited[r + 1][c] = true;
-			dfs(grid, visited, r + 1, c);
-		}
-		if (c - 1 >= 0 && visited[r][c - 1] == false && grid[r][c - 1] == '1')
-		{
-			visited[r][c - 1] = true;
-			dfs(grid, visited, r, c - 1);
-		}
-		if (c + 1 < grid[0].size() && visited[r][c + 1] == false && grid[r][c + 1] == '1')
-		{
-			visited[r][c + 1] = true;
-			dfs(grid, visited, r, c + 1);
-		}*/
 	}
 };
