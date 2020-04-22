2d12ea5eb5450238ab6e2c2670fcc34e57bfb9fd
Yashraj Baheti <35994639+yashraj1120@users.noreply.github.com>
2020-04-22 13:27:45 +0530

    refactoring backspacestringcompare.cpp

--- 30 Day Challenge/Week 2/backspacestringcompare.cpp
+++ 30 Day Challenge/Week 2/backspacestringcompare.cpp
@@ -1,92 +1,88 @@
 class Solution
 {
 public:
-    bool backspaceCompare(string S, string T)
-    {
-        int s = S.length() - 1;
-        int t = T.length() - 1;
-        int ss = 0;
-        int ts = 0;
-        while (s >= 0 && t >= 0)
-        {
-            //cout<<S[s]<<" "<<T[t]<<endl;
-            while (s >= 0 && (ss || S[s] == '#'))
-            {
-                if (S[s] == '#')
-                {
-                    ss++;
-                    s--;
-                }
-                else if (ss)
-                {
-                    ss--;
-                    s--;
-                }
-            }
-            while (t >= 0 && (ts || T[t] == '#'))
-            {
-                if (T[t] == '#')
-                {
-                    ts++;
-                    t--;
-                }
-                else if (ts)
-                {
-                    ts--;
-                    t--;
-                }
-            }
-            //cout<<S[s]<<" "<<T[t]<<endl;
-            if (t >= 0 && s >= 0 && !ss && !ts && T[t] != S[s])
-            {
-                return false;
-            }
-            if (!ss && !ts && s >= 0 && t >= 0)
-            {
-                s--;
-                t--;
-            }
-        }
+	bool backspaceCompare(string S, string T)
+	{
+		int s = S.length() - 1;
+		int t = T.length() - 1;
+		int ss = 0;
+		int ts = 0;
+		while (s >= 0 && t >= 0)
+		{
+			//cout<<S[s]<<" "<<T[t]<<endl;
+			while (s >= 0 && (ss || S[s] == '#'))
+			{
+				if (S[s] == '#')
+				{
+					ss++;
+				}
+				else if (ss)
+				{
+					ss--;
+				}
+				s--;
+			}
+			while (t >= 0 && (ts || T[t] == '#'))
+			{
+				if (T[t] == '#')
+				{
+					ts++;	
+				}
+				else if (ts)
+				{
+					ts--;
+				}
+				t--;
+			}
+			//cout<<S[s]<<" "<<T[t]<<endl;
+			if (s >= 0 && t >= 0 && !ss && !ts)
+			{
+				if (T[t] != S[s])
+				{
+					return false;
+				}
+				s--;
+				t--;
+			}
+		}
 
-        //cout<<"s :"<<s<<" t: "<<t<<endl;
-        int ls = s;
-        while (s >= 0)
-        {
-            if (S[s] == '#')
-            {
-                ss++;
-                s--;
-            }
-            else if (ss)
-            {
-                ss--;
-                s--;
-            }
-            else
-            {
-                return false;
-            }
-        }
+		//cout<<"s :"<<s<<" t: "<<t<<endl;
+		int ls = s;
+		while (s >= 0)
+		{
+			if (S[s] == '#')
+			{
+				ss++;
+			}
+			else if (ss)
+			{
+				ss--;
+			}
+			else
+			{
+				return false;
+			}
+			s--;
+		}
 
-        int tl = t;
-        while (t >= 0)
-        {
-            if (T[t] == '#')
-            {
-                ts++;
-                t--;
-            }
-            else if (ts)
-            {
-                ts--;
-                t--;
-            }
-            else
-            {
-                return false;
-            }
-        }
+		int tl = t;
+		while (t >= 0)
+		{
+			if (T[t] == '#')
+			{
+				ts++;
+			}
+			else if (ts)
+			{
+				ts--;
+			}
+			else
+			{
+				return false;
+			}
+			t--;
+		}
 
-        return true;
-    }
+		return true;
+	}
 };
