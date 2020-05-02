dbf1e66b52fbf429cb8b76bd4fe5df43c0675666
Yashraj Baheti <35994639+yashraj1120@users.noreply.github.com>
2020-05-02 12:05:28 +0530

    week 1 problem 1

---
+++ 30 Day Challenge May/Week1/FirstBadVersion.cpp
@@ -0,0 +1,24 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+	int firstBadVersion(int n) {
+		int l = 1;
+		int h = n;
+		while (l != h)
+		{
+			int mid = l + (h - l) / 2;
+			bool curr = isBadVersion(mid);
+			if (curr)
+			{
+				h = mid;
+			}
+			else
+			{
+				l = mid + 1;
+			}
+		}
+		return l;
+	}
+};
